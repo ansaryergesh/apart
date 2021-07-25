400e6985b47a493271f24ae1cd0436ebc0848f9c
ansaryergesh <yergeshovansar@gmail.com>
2021-07-26 04:13:57 +0600

    prod1

---
+++ precache-manifest.b25cf98159f28a66311af875e26f72a2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b7762172917c6cb5744a5cb66ad57215",
+    "url": "/react/realdeal/index.html"
+  },
+  {
+    "revision": "875e28fced6ff598a70f",
+    "url": "/react/realdeal/static/js/2.f7c4b3a5.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react/realdeal/static/js/2.f7c4b3a5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a5e8a778661c5c4cb399",
+    "url": "/react/realdeal/static/js/main.25c53321.chunk.js"
+  },
+  {
+    "revision": "efe4249b9b416f57cb3f",
+    "url": "/react/realdeal/static/js/runtime-main.524a4886.js"
+  }
+]);
